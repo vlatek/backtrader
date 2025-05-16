6bbbea27f25436235f5468ed66080b290d4a28b8
vt <vlatek7@gmail.com>
2025-05-16 15:00:45 +0200

    Change supported Python version up to 3.11

--- setup.py
+++ setup.py
@@ -91,6 +91,10 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
 
         # Operating Systems on which it runs
         'Operating System :: OS Independent',
